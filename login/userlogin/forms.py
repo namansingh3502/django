11f02ad50967654f7d0735a5d42b8c1a9c278a1b
NAMAN <namansingh0305@gmail.com>
2020-08-05 02:17:37 +0530

    error

--- login/userlogin/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UserPass( forms.Form ):
-	username = forms.CharField( label='Username')
-	password = forms.CharField( widget=forms.PasswordInput, label='Password')
--- login/userlogin/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Userdata( models.Model ):
-	username = models.CharField(max_length = 10)
-	password = models.CharField(max_length = 10)
